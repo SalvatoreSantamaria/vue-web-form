2c5225f2337f97d7e9447161c484a6fb7b28f56b
Salvatore Santamaria <sam.santamaria@gmail.com>
2021-02-04 15:02:56 -0800

    Initial Project Creation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <signupform>
+
+    https://youtu.be/ixOcve5PX-Q?t=275
+  </signupform>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Signupform from './components/signupform.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Signupform
+
   }
 }
 </script>
@@ -22,5 +26,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
 }
 </style>
